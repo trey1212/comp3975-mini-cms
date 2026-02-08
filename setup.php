cad1c9a95515476f55da96cada500335c29388f2
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2026-02-07 20:14:39 -0800

    Added timestamp columns for articles

--- setup.php
+++ setup.php
@@ -39,6 +39,8 @@ $article = "
         id INT NOT NULL AUTO_INCREMENT,
         title VARCHAR(40) NOT NULL,
         content TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         PRIMARY KEY (id)
     );
 
